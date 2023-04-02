955f0c75357eb72617bc52e35d0a2b8728d757df
popojk <c.alexwu@gmail.com>
2023-04-02 12:36:38 +0800

    add readme

--- models/seeds/urlSeeders.js
+++ models/seeds/urlSeeders.js
@@ -2,6 +2,7 @@ const Url = require('../url') // 載入 todo model
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  Url.create({ originalUrl: 'aaa', shortUrl: 'bbb' })
-    console.log('done')
+  Url.create({ originalUrl: 'https://getbootstrap.com/', shortUrl: 'DF3wS' })
+  Url.create({ originalUrl: 'https://developer.mozilla.org/en-US/', shortUrl: 'FJ22I' })
+  console.log('done')
 })
